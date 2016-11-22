f46d49c5ec4789f83bf342baa0bf64c013d1d7ae
Chandra Wannigama <Chandra Wannigama>
2016-11-23 00:32:25 +1300

    started reservation backend

---
+++ FabrikamFood/FabrikamFood/AzureManager.cs
@@ -0,0 +1,46 @@
+﻿using FabrikamFood.DataModels;
+using Microsoft.WindowsAzure.MobileServices;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FabrikamFood
+{
+    public class AzureManager
+    {
+        private static AzureManager instance;
+        private MobileServiceClient client;
+        private IMobileServiceTable<Reservation> reservationTable;
+
+        private AzureManager()
+        {
+            this.client = new MobileServiceClient("MOBILE_APP_URL");
+            this.reservationTable = this.client.GetTable<Reservation>();
+        }
+
+        public MobileServiceClient AzureClient
+        {
+            get { return client; }
+        }
+
+        public static AzureManager AzureManagerInstance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    instance = new AzureManager();
+                }
+
+                return instance;
+            }
+        }
+
+        public async Task<List<Reservation>> GetReservations()
+        {
+            return await this.reservationTable.ToListAsync();
+        }
+    }
+}
---
+++ FabrikamFood/FabrikamFood/DataModels/Reservation.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.WindowsAzure.MobileServices;
+using Newtonsoft.Json;
+
+namespace FabrikamFood.DataModels
+{
+    public class Reservation
+    {
+        [JsonProperty(PropertyName = "Id")]
+        public string ID { get; set; }
+
+        [JsonProperty(PropertyName = "name")]
+        public string Name { get; set; }
+
+        [JsonProperty(PropertyName = "phone")]
+        public string Phone { get; set; }
+
+        [JsonProperty(PropertyName = "date")]
+        public string Date { get; set; }
+
+        [JsonProperty(PropertyName = "time")]
+        public string Time { get; set; }
+
+
+
+
+
+    }
+}
